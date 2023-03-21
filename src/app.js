87dfbfe3d013e8992a940ca4c1ee2059531b088b
Daniel López <dani.lopezgonzalez.94@gmail.com>
2023-03-21 14:17:29 +0000

    Web interaction ready

--- src/app.js
+++ src/app.js
@@ -1,11 +1,47 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("My first JS project!");
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = excuseGenerator();
+  });
+
+  let randomNumber = () => {
+    let aleatorio = Math.floor(Math.random() * 4);
+    return aleatorio;
+  };
+
+  let excuseGenerator = () => {
+    let who = ["My cat", "My cousin", "Your raccoon", "The president"];
+    let action = ["ate", "shot at", "crushed", "smashed"];
+    let what = [
+      "my homework",
+      "my computer",
+      "my dreams",
+      "the concept of time"
+    ];
+    let when = [
+      "before lunch",
+      "right on time",
+      "during my sabbatical year",
+      "while I was taking a nap"
+    ];
+
+    return (
+      who[randomNumber()] +
+      " " +
+      action[randomNumber()] +
+      " " +
+      what[randomNumber()] +
+      " " +
+      when[randomNumber()] +
+      "."
+    );
+  };
+
+  document.querySelector("#excuse").innerHTML = excuseGenerator();
 };
